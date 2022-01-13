cd8da9f9c708266442185d84a5f3e848ae5bb581
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-13 15:01:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log('JS OK!')
+
+// ! Snack 1
+
+/* Il software deve chiedere 5 volte all'utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while. */
+
+// ! Snack 2
+
+/* Inserisci un numero, se è pari stampa il numero, se è dispari stampa in numero successivo */
+
+// ! Snack 3 
+
+/* Generatori di "nomi cognomi" casuali: prendendo una lista di nomi e una lista di cognomi,
+Gatsby vuole generare una lista di 3 invitati. */
+
+// ! Snack 4
+
+/* Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari */
+
+// ! Snack 5
+
+/* Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all'array che hanno meno elementi,
+fino a quando ne avrà tanti quanto l'altro. */
+
